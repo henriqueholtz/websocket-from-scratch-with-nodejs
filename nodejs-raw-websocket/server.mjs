c8d4f2f73d23201b6986d4754a9df6944507faa5
henriqueholtz <henrique_holtz@hotmail.com>
2022-07-27 07:57:23 -0300

    chore: server -> implement to receive messages with still 16-bits

--- nodejs-raw-websocket/server.mjs
+++ nodejs-raw-websocket/server.mjs
@@ -8,6 +8,8 @@ const WEBSOCKET_MAGIC_STRING_KEY = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 const SEVEN_BITS_INTEGER_MARKER = 125;
 const SIXTEEN_BITS_INTEGER_MARKER = 126;
 const SIXTYFOUR_BITS_INTEGER_MARKER = 127;
+
+const MAXIMUM_SIXTEEN_BITS_INTEGER = 2 ** 16; // 0 to 65536
 const MASK_KEY_BYTES_LENGTH = 4;
 const OPCODE_TEXT = 0x01; // 1 bit in binary
 
@@ -39,14 +41,29 @@ function prepareMessage(message) {
   const messageSize = msg.length;
 
   let dataFrameBuffer;
-  let offset = 2;
 
   // 0x80 === 128 in binary
   // '0x' + Math.abs(128).toString(16) === 0x80
   const firstByte = 0x80 | OPCODE_TEXT; // single frame + text
+
   if (messageSize <= SEVEN_BITS_INTEGER_MARKER) {
     const bytes = [firstByte];
     dataFrameBuffer = Buffer.from(bytes.concat(messageSize));
+  } else if (messageSize <= MAXIMUM_SIXTEEN_BITS_INTEGER) {
+    let offsetFoursBytes = 4;
+    const target = Buffer.allocUnsafe(offsetFoursBytes);
+    target[0] = firstByte;
+    target[1] = SIXTEEN_BITS_INTEGER_MARKER | 0x0; //just to know the mask
+
+    target.writeUint16BE(messageSize, 2); // content length is 2 bytes
+    dataFrameBuffer = target;
+
+    // alloc 4 bytes
+    // [0] -> 128 + 1 -> 10000001 = 0x01 fin + opcode
+    // [1] -> 126 + 0 -> payload length marker + mask indicator
+    // [2] -> 0 -> content length
+    // [3] -> 140 -> content length
+    // [4 - ...] -> the message itself
   } else {
     throw new Error("Message too long buddy :( ");
   }
@@ -81,6 +98,9 @@ function onSocketReadable(socket) {
   let messageLength = 0;
   if (lengthIndicatorInBits <= SEVEN_BITS_INTEGER_MARKER) {
     messageLength = lengthIndicatorInBits;
+  } else if (lengthIndicatorInBits === SIXTEEN_BITS_INTEGER_MARKER) {
+    // unsigned, big-endian 16-bit integer [0 - 65k] - 2 ** 16
+    messageLength = socket.read(2).readUint16BE(0); //2 bytes
   } else {
     throw new Error(`Your message is too long! We don't handle 64-bit messages`);
   }
@@ -93,11 +113,10 @@ function onSocketReadable(socket) {
   const data = JSON.parse(received);
   console.log("message received: ", data);
 
-  // const msg = JSON.stringify({
-  //   message: data,
-  //   at: new Date().toISOString(),
-  // });
-  const msg = JSON.stringify(data);
+  const msg = JSON.stringify({
+    message: data,
+    at: new Date().toISOString(),
+  });
   sendMessage(msg, socket);
 }
 
